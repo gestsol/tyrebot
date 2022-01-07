274f650728bb5e355536637e0c8b00d98a0ea8e0
Romulo <romulocg25@gmail.com>
2022-01-07 18:23:47 -0400

    Fix redirection

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './pages/main/main.component';
 
 const routes: Routes = [
-	{path:'', component: MainComponent},
+	{path:'', redirectTo:'active-vehicle', pathMatch: 'full'},
 	{path:'add-vehicle', loadChildren: () => import('./pages/add-vehicle/add-vehicle-routing.module').then( m => m.AddVehicleRoutingModule)},
 	{path:'active-vehicle', loadChildren: () => import('./pages/active-vehicles/active-vehicles.module').then( m => m.ActiveVehiclesModule)},
 	{path:'**', redirectTo:'', pathMatch: 'full'},
