cee817bd91bb5248f37adaf9c007863c2259b1b9
Romulo <romulocg25@gmail.com>
2022-02-08 12:33:19 -0400

    Add No Signal indicator

--- src/app/components/bus/axie/axie.component.ts
+++ src/app/components/bus/axie/axie.component.ts
@@ -1,20 +1,37 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { SvgIconComponent } from 'angular-svg-icon';
 
 @Component({
   selector: 'app-axie',
   templateUrl: './axie.component.html',
   styleUrls: ['./axie.component.scss']
 })
-export class AxieComponent implements OnInit {
+export class AxieComponent implements AfterViewInit {
   @Input() axie = 1
   @Input() type = 2
   @Input() tyres: any[] = []
+  @ViewChild(SvgIconComponent) axieTemplateRef!: any
+  intervals: number[] = []
   classes: string[] = []
 
   constructor() { }
 
-  ngOnInit(): void {
-    this.classes = this.tyres.map((item, i) => item.state? `${item.state}-${i + 1}`: '')
+  ngAfterViewInit(): void {
+    this.tyres.forEach((item, i) => {
+      const tyre = i + 1
+      const newClass = item.state? `${item.state}-${tyre}`: ''
+      if (newClass.includes('NO_SIGNAL')) {
+        const interval = setInterval(() => {
+          const node = this.axieTemplateRef.element.nativeElement?.querySelector(`.${newClass} #Tire_${tyre} text`)
+          if (node) {
+            node.innerHTML = 'Sin Señal'
+            node.setAttribute('transform', 'translate(68 352.62)')
+            clearInterval(interval)
+          }
+        }, 100)
+      }
+      this.classes.push(newClass)
+    })
   }
 
 }
--- src/app/components/bus/replacement/replacement.component.ts
+++ src/app/components/bus/replacement/replacement.component.ts
@@ -1,16 +1,30 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, ViewChild } from '@angular/core';
+import { SvgIconComponent } from 'angular-svg-icon';
 
 @Component({
   selector: 'app-replacement',
   templateUrl: './replacement.component.html',
   styleUrls: ['./replacement.component.scss']
 })
-export class ReplacementComponent implements OnInit {
+export class ReplacementComponent implements AfterViewInit {
   @Input() type: number = 1;
   @Input() state: string = '';
+  @ViewChild(SvgIconComponent) replaceTemplateRef!: any
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    if (this.state.includes('NO_SIGNAL')) {
+      const interval = setInterval(() => {
+        const nodes = this.replaceTemplateRef.element.nativeElement?.querySelectorAll(`.${this.state} #Tire_${this.type}R text`)
+        if (nodes?.length) {
+          nodes[0].innerHTML = 'Sin'
+          nodes[0].setAttribute('transform', 'translate(64.5 66.07)')
+          nodes[1].innerHTML = 'Señal'
+          nodes[1].setAttribute('transform', 'translate(58.05 82.07)')
+          clearInterval(interval)
+        }
+      }, 100)
+    }
   }
 
 }
