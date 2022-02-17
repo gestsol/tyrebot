c27649991493ae75514f41f66b67fe09bd91f3d6
Romulo <romulocg25@gmail.com>
2022-02-17 14:56:57 -0400

    Add no signal 48 label in vehicle detail

--- src/app/components/bus/axie/axie.component.ts
+++ src/app/components/bus/axie/axie.component.ts
@@ -21,22 +21,27 @@ export class AxieComponent implements AfterViewInit {
     this.tyres.forEach((item, i) => {
       const tyre = i + 1
       const newClass = item.state? `${item.state}-${tyre}`: ''
-      if (newClass.includes(TyreState.NoSignal) || newClass.includes(TyreState.NoSignal48)) {
-        const interval = setInterval(() => {
-          const node = this.axieTemplateRef.element.nativeElement?.querySelector(`.${newClass} #Tire_${tyre} text`)
-          if (node) {
-            node.innerHTML = 'Sin Señal'
-            const attribute = node.getAttribute('transform') as string
-            const splited = attribute.split(' ')
-            splited[0] = 'translate(68'
-            const newAttribute = splited.join(' ')
-            node.setAttribute('transform', newAttribute)
-            clearInterval(interval)
-          }
-        }, 100)
-      }
+      this.insertNoSignalText(newClass, tyre, TyreState.NoSignal)
+      this.insertNoSignalText(newClass, tyre, TyreState.NoSignal48)
       this.classes.push(newClass)
     })
+  }
+
+  insertNoSignalText(className: string, tyre: number, state: TyreState) {
+    if (className.includes(state)) {
+      const interval = setInterval(() => {
+        const node = this.axieTemplateRef.element.nativeElement?.querySelector(`.${className} #Tire_${tyre} text`)
+        if (node) {
+          node.innerHTML = state === TyreState.NoSignal ? 'Sin Señal' : 'Sin Señal 48h'
+          const attribute = node.getAttribute('transform') as string
+          const splited = attribute.split(' ')
+          splited[0] = `translate(${state === TyreState.NoSignal? '68': '48'}`
+          const newAttribute = splited.join(' ')
+          node.setAttribute('transform', newAttribute)
+          clearInterval(interval)
+        }
+      }, 100)
+    }
   }
 
 }
--- src/app/components/bus/replacement/replacement.component.ts
+++ src/app/components/bus/replacement/replacement.component.ts
@@ -14,18 +14,22 @@ export class ReplacementComponent implements AfterViewInit {
   constructor() { }
 
   ngAfterViewInit(): void {
-    if (this.state.includes(TyreState.NoSignal) || this.state.includes(TyreState.NoSignal48)) {
+    this.insertNoSignalText(TyreState.NoSignal)
+    this.insertNoSignalText(TyreState.NoSignal48)
+  }
+
+  insertNoSignalText(state: TyreState) {
+    if (this.state.includes(state)) {
       const interval = setInterval(() => {
         const nodes = this.replaceTemplateRef.element.nativeElement?.querySelectorAll(`.${this.state} #Tire_${this.type}R text`)
         if (nodes?.length) {
-          nodes[0].innerHTML = 'Sin'
-          nodes[0].setAttribute('transform', 'translate(64.5 66.07)')
-          nodes[1].innerHTML = 'Señal'
-          nodes[1].setAttribute('transform', 'translate(58.05 82.07)')
+          nodes[0].innerHTML = state === TyreState.NoSignal? 'Sin': 'Sin Señal'
+          nodes[0].setAttribute('transform', `translate(${state === TyreState.NoSignal? '64.5': '46.5'} 66.07)`)
+          nodes[1].innerHTML = state === TyreState.NoSignal? 'Señal': '48h'
+          nodes[1].setAttribute('transform', `translate(${state === TyreState.NoSignal? '58.05': '61.05'} 82.07)`)
           clearInterval(interval)
         }
       }, 100)
     }
   }
-
 }
