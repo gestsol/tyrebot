c573f1d8952b16617fc7caef4643589f335545be
Romulo <romulocg25@gmail.com>
2022-02-24 16:46:25 -0400

    Adding users list

--- src/app/pages/manage-accounts/manage-accounts-list/manage-accounts-list.component.ts
+++ src/app/pages/manage-accounts/manage-accounts-list/manage-accounts-list.component.ts
@@ -3,6 +3,8 @@ import {AfterViewInit, Component, ViewChild} from '@angular/core';
 import {MatPaginator} from '@angular/material/paginator';
 import {MatTableDataSource} from '@angular/material/table';
 import { ActivatedRoute, Router } from '@angular/router';
+import * as moment from 'moment';
+import { UserService } from 'src/app/services/user.service';
 
 enum State {
   access = 'Acceso',
@@ -10,29 +12,12 @@ enum State {
 }
 
 
-export interface PeriodicElement {
-  name: string;
-  last_connection: number;
-  state: State;
+export interface TableItem {
+  email: string;
+  last_login: string;
+  active: boolean;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {name: 'Hydrogen', last_connection: 1, state: State.access},
-  {name: 'Helium', last_connection: 2, state: State.access},
-  {name: 'Lithium', last_connection: 3, state: State.access},
-  {name: 'Beryllium', last_connection: 4, state: State.access},
-  {name: 'Boron', last_connection: 5, state: State.access},
-  {name: 'Carbon', last_connection: 6, state: State.access},
-  {name: 'Nitrogen', last_connection: 7, state: State.access},
-  {name: 'Oxygen', last_connection: 8, state: State.denied},
-  {name: 'Fluorine', last_connection: 9, state: State.access},
-  {name: 'Neon', last_connection: 10, state: State.access},
-  {name: 'Sodium', last_connection: 11, state: State.access},
-  {name: 'Magnesium', last_connection: 12, state: State.denied},
-  {name: 'Aluminum', last_connection: 13, state: State.access},
-  {name: 'Silicon', last_connection: 14, state: State.access},
-  {name: 'Phosphorus', last_connection: 15, state: State.access}
-];
 
 @Component({
   selector: 'app-manage-accounts-list',
@@ -41,16 +26,27 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 export class ManageAccountsListComponent implements AfterViewInit {
   displayedColumns: string[] = ['name', 'last_connection', 'state'];
-  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+  dataSource = new MatTableDataSource<TableItem>();
 
   states = State
 
   constructor(
     private router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private userService: UserService
   ) {}
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  ngOnInit() {
+    this.userService.getAll().subscribe((list) => {
+      this.dataSource.data = list.map((item) => ({
+        email: item.email,
+        last_login: item.last_login? moment(item.last_login, 'YYYY-MM-DDTHH:mm:ss').format('DD/MM/YYYY HH:mm:ss') : 'N/A',
+        active: item.active
+      }))
+    })
+  }
 
   ngAfterViewInit() {
     this.paginator._formFieldAppearance = 'outline'
